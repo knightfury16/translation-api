e73419859504764ff3c5ed1224ddf90f55bd0df7
suhaib <suhaib.ahmmad@orbitax.com>
2024-02-22 19:13:44 +0600

    created a route to test the translator service.

---
+++ translation.api/Controllers/TranslationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using translation.application;
+using translation.domain.Entity;
+using translation.domain.Enum;
+using translation.domain.Translation;
+
+namespace translation.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TranslationController : ControllerBase
+    {
+
+        public ITranslator _translator;
+
+        public TranslationController(ITranslator translator)
+        {
+            _translator = translator;
+        }
+        [HttpGet]
+        public Translation GetTranslation()
+        {
+            OriginalMessage originalMessage = new OriginalMessage("Hello there my friend");
+            return _translator.Translate(originalMessage, TranslationServiceType.Shakespear);
+        }
+    }
+}
--- translation.api/Program.cs
+++ translation.api/Program.cs
@@ -1,3 +1,6 @@
+using translation.application;
+using translation.infrastructure.TranslatorServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ITranslator,Translator>();
+builder.Services.AddScoped<ITranslatorService, TranslatorService>();
+
 
 var app = builder.Build();
 
