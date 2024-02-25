97db47ef0a22c14cf39e0ec26c48bfa82a1f60ab
knightfury16 <suhaibknight@gmail.com>
2024-02-25 14:22:27 +0600

    Matched the service to new objects.

--- translation.infrastructure/TranslatorServices/TranslatorService.cs
+++ translation.infrastructure/TranslatorServices/TranslatorService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using translation.application;
+using translation.application.Common.Interfaces;
 using translation.domain;
 using translation.domain.Entity;
 using translation.domain.Enum;
@@ -15,12 +15,13 @@ namespace translation.infrastructure.TranslatorServices
     {
        
 
-        public Translation Translate(OriginalMessage originalMessage, TranslationServiceType translationServiceType)
+        public Translation Translate(string message, TranslationServiceType translationServiceType)
         {
             //call the translation service here
             // for now 
+            OriginalMessage originalMsg = new OriginalMessage(message);
             TranslatedMessage translatedMessage = new TranslatedMessage("Valorous morrow to thee, sir thither mine cousin", translationServiceType);
-            Translation translation = new Translation(originalMessage, translatedMessage);
+            Translation translation = new Translation(originalMsg, translatedMessage);
 
             return translation;
         }
