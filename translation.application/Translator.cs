f24f7b41c391c3b8a5fb4b588e6e812b19fe7621
knightfury16 <suhaibknight@gmail.com>
2024-02-25 14:21:33 +0600

    Modified the Translator to the request object

--- translation.application/Translator.cs
+++ translation.application/Translator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using translation.application.Common.Interfaces;
+using translation.application.Common.Models;
 using translation.domain;
 using translation.domain.Entity;
 using translation.domain.Enum;
@@ -15,19 +17,26 @@ namespace translation.application
 
         private readonly ITranslatorService _translatorService;
 
-        public Translator(ITranslatorService translatorServie)
+        public Translator(ITranslatorService translatorService)
         {
-            _translatorService = translatorServie;
+            _translatorService = translatorService;
         }
 
-        public Translation AutoTranslate(Message originalText)
+        public Translation Translate(TranslateRequestDto request)
+        {
+            //unpacking
+            string message = request.message.ToString();
+            TranslationServiceType translationService = request.translationServiceType;
+
+            //calling the service
+            var translation = _translatorService.Translate(message, translationService);
+
+            return translation;
+        }
+
+        public Translation AutoTranslate(TranslateRequestDto request)
         {
             throw new NotImplementedException();
-        }
-
-        public Translation Translate(OriginalMessage originalText, TranslationServiceType translationService)
-        {
-            return _translatorService.Translate(originalText, translationService);
         }
     }
 }
