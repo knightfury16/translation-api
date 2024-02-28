98ccea4030527322d4bcb30dc6d22ae02a6d16e7
knightfury16 <suhaibknight@gmail.com>
2024-02-28 11:45:57 +0600

    Created available language class

---
+++ translation.infrastructure/TranslatorServices/AvailableLanguage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using translation.domain.Entity;
+
+namespace translation.infrastructure.TranslatorServices
+{
+    public class AvailableLanguage : IAvailableLanguage
+    {
+        public Language CheckLanguageAvailability(string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
